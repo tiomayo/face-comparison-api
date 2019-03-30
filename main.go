f1012b46f589492591ff7f7d01f9befadf33637c
Adhitya Yosua Sanjaya <adhityasanit@gmail.com>
2019-03-30 21:13:54 +0700

    update main for gracefull shutdown

--- main.go
+++ main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"context"
+	"errors"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/tiomayo/face-comparison-api/controller"
@@ -13,36 +17,74 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func init() {
-	connectMongo()
-}
-
 func main() {
 	r := mux.NewRouter()
+
 	http.Handle("/", r)
+
+	srv := &http.Server{
+		Handler:      r,
+		Addr:         ":8000",
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	// Define Routes
 	r.HandleFunc("/identify", controller.Identify).Methods("POST")
 	r.HandleFunc("/go/aisatsu", controller.Aisatsu).Methods("GET")
 	// r.HandleFunc("/upload", controller.UploadImage).Methods("POST")
-	log.Println("Connected to port 8000")
-	if err := http.ListenAndServe(":8000", r); err != nil {
-		log.Fatal(err)
-	}
+
+	// Start Server
+	go func() {
+
+		log.Printf("Starting Server at %v...\n", srv.Addr)
+		if errsrv := srv.ListenAndServe(); errsrv != nil {
+			log.Fatal(errsrv)
+		}
+
+		errdb := dbConnection()
+		if errdb != nil {
+			log.Fatal(errdb)
+		}
+
+	}()
+
+	// Graceful Shutdown
+	waitShutdownSignal(srv)
 }
 
-func connectMongo() bool {
-	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+func dbConnection() error {
+
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
-	if err != nil {
-		log.Fatal(err)
+	client, errCreateClient := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+
+	if errCreateClient != nil {
+		return errors.New("Fail create new mongo client")
 	}
 
-	errConnect := client.Connect(ctx)
+	errClientConnect := client.Connect(ctx)
 
-	if errConnect != nil {
-		return false
+	if errClientConnect != nil {
+		return errors.New("Mongo client fail to connect")
 	}
 
-	return true
+	return nil
+}
+
+func waitShutdownSignal(srv *http.Server) {
+	interruptChan := make(chan os.Signal, 1)
+	signal.Notify(interruptChan, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	// Block until we receive our signal.
+	<-interruptChan
+
+	// Create a deadline to wait for.
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+	srv.Shutdown(ctx)
+
+	log.Println("Shutting down...")
+	os.Exit(0)
 }
